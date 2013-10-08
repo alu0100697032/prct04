3ead1be3b8abeeda8fd06d7eb8b910dc2aa2b1ad
Victor Hernandez <alu0100697032@ull.edu.es>
2013-10-08 17:01:53 +0100

    Definicion de la funcion multiplicacion

---
+++ matriz.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+a = [[1,1,1],[2,2,2],[3,3,3]]
+b = [[4,4,4],[5,5,5],[6,6,6]]
+c = [[0,0,0],[0,0,0],[0,0,0]]
+
+def multiplicacion(a,b,c)
+	m,n = a.size,a[1].size
+	for y in 0...m do
+		for i in 0...m do
+			auxiliar = 0
+				for j in 0...n do
+					auxiliar += a[i][j]*b[j][y]
+				end	
+		c[i][y] = auxiliar;
+		end
+	end
+
+puts "Multiplicación:"
+
+
+	for i in 0...m do
+		for j in 0...n do
+			print c[i][j]
+			print " "
+		end
+	puts ""
+	end
+end
+
